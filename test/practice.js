2ea3f22129c9b6590a353c9768db5389d3aaf859
ali <kemal_tar@hotmail.com>
2023-02-19 19:12:43 -0600

    selenium file updated with delete cookies function

--- test/practice.js
+++ test/practice.js
@@ -1,6 +1,7 @@
 const { Builder, By } = require('selenium-webdriver');
 const { DevTools } = require('selenium-adapter');
 const assert = require('assert');
+
 require("chromedriver");
 
 
@@ -18,80 +19,92 @@ describe('DevTools Tests', async function () {
     // Close the DevTools session and WebDriver instance after each test
     afterEach(async function () {
         //await devTools.close();
-       // await driver.quit();
+        // await driver.quit();
     });
 
     it('Navigate to the website', async function () {
 
-        //   try {
-        // Navigate to a URL
-        await driver.get('https://www.idealimage.com/');
+        try {
+            // Navigate to a URL
+            await driver.get('https://www.idealimage.com/');
 
-        await driver.manage().window().maximize();
+            // await driver.manage().window().maximize();
 
-        await driver.wait(() => driver.executeScript('return document.readyState').then(state => state === 'complete'), 50000);
+            await driver.wait(() => driver.executeScript('return document.readyState').then(state => state === 'complete'), 50000);
 
-        await driver.findElement(By.id('FC_Nav')).click();
-        await driver.get('https://www.idealimage.com/schedule')
+            await driver.findElement(By.id('getStart')).click();
+            //await driver.get('https://www.idealimage.com/schedule')
 
-        // const currentUrl = await driver.getCurrentUrl();
-        // assert.equal(currentUrl, 'https://www.idealimage.com/free-consultation?c=1')
+            // const currentUrl = await driver.getCurrentUrl();
+            // assert.equal(currentUrl, 'https://www.idealimage.com/free-consultation?c=1')
 
-        //await driver.executeScript('prospect.metas.flow')
+            //await driver.executeScript('prospect.metas.flow')
 
-        const variable = await driver.executeScript('return window.sessionStorage')
-        //console.log(variable)
+            const variable = await driver.executeScript('return window.sessionStorage')
+            //console.log(variable)
 
-        await driver.navigate().refresh();
-        await driver.manage().getCookie('prospect_id').then(function(cookie){
-            console.log(`prospect id is : ${cookie.value}`);
-        })
-
-        await driver.manage().deleteCookie('prospect_id');
-
-        // await driver.manage().getCookie('prospect_id').then(function(cookie){
-        //     console.log(`prospect id is : ${cookie.value}`);
-        // })
-
-
-        
-
-        
+            
 
 
 
+            await driver.manage().getCookie('prospect_id').then(function (cookie) {
+                console.log(`prospect id is before refresh or clear cookies : ${cookie.value}`);
+            })
+
+            //await driver.manage().deleteCookie('prospect_id');
+            await driver.manage().deleteAllCookies();
+
+            await driver.sleep(5000);
 
 
-        //    for(let key in variable){
-        //     if(key.includes('prospectId')){
-        //         console.log(variable[key]);
 
-        //     }
-        //    }
+            //await driver.navigate().refresh();\
+            //await driver.executeScript('location.reload(true);')
+            //await driver.executeScript('window.sessionStorage.clear()');
+
+            await driver.manage().getCookie('prospect_id').then(function (cookie) {
+                console.log(`prospect id after refresh or clear cookies : ${cookie.value}`);
+            })
 
 
 
 
-        // const value =await driver.executeScript('return window.prospect.metas')
-        // console.log(value.serviceTypeCode.value)
-        //     let actual=value.flow.value;
-        //     let expected='organic-botox-ss-treatment'
-        //    if(actual == 'organic-botox-ss-treatment'){
-        //     console.log('this is SS-botox flow')
-        //    }
-        //    else if(actual == 'organic'){
-        //     console.log('this is free consultation flow')
-        //    }
-        //    else{
-        //     console.log("Undefined flow")
-        //    }
-        //     assert.equal(actual,expected)
 
 
-        // } catch (error) {
-        //     console.error(`Error occured: ${error}`)
 
-        // }
+
+
+
+            //    for(let key in variable){
+            //     if(key.includes('prospectId')){
+            //         console.log(variable[key]);
+
+            //     }
+            //    }
+
+
+
+
+            // const value =await driver.executeScript('return window.prospect.metas')
+            // console.log(value.serviceTypeCode.value)
+            //     let actual=value.flow.value;
+            //     let expected='organic-botox-ss-treatment'
+            //    if(actual == 'organic-botox-ss-treatment'){
+            //     console.log('this is SS-botox flow')
+            //    }
+            //    else if(actual == 'organic'){
+            //     console.log('this is free consultation flow')
+            //    }
+            //    else{
+            //     console.log("Undefined flow")
+            //    }
+            //     assert.equal(actual,expected)
+
+
+        } catch (error) {
+            console.error(`Error occured: ${error}`)
+
+        }
 
 
 
